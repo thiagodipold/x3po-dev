38e08d850520096e06698f8f18eb95806ea3d2ce
Thiago Dipold <thiagodipcavalcanti@gmail.com>
2020-09-09 10:44:39 -0300

    pasta assets

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,15 @@
+// const element = document.querySelector("#saiba-mais");
+
+
+// // smooth scroll to element and align it at the bottom
+// element.scrollIntoView({ behavior: 'smooth', block: 'end'});
+
+// document.querySelector('#saiba-mais').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     let elmnt = document.getElementById('nosso-proposito');
+//     elmnt.scrollIntoView();
+//   });
+
+
+
+
